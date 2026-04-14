3ddcb41e70fa1478e29874afe4c7041adacd8f3f
Clement <clement.fontan@outlook.com>
2026-04-15 00:00:17 +0200

    mieux en rusanov

--- first_steps/FV_ext.F90
+++ first_steps/FV_ext.F90
@@ -7,7 +7,13 @@ function flux(u_) result(f)
    return
 end function flux
 
-
+function flux_p(u_) result(f)
+   implicit none
+   real, intent(in)  :: u_
+   real              :: f
+   f = u_
+   return
+end function flux_p
 
 function godunov(u_,v_)
    implicit none
@@ -17,9 +23,21 @@ function godunov(u_,v_)
 
    if(u_>v_) then
       godunov = min(flux(u_), flux(v_))
+      if(u_*v_<=0) godunov =0
    else 
       godunov = max(flux(u_), flux(v_))
-      if(u_*v_<=0) godunov =0
    end if
    return
 end function godunov
+
+
+function rusanov(u_,v_)
+   implicit none
+   real, intent(in)  :: u_,v_
+   real              :: rusanov
+   real              :: flux, flux_p
+
+   rusanov = 0.5*(flux(u_)+flux(v_))-0.5* max(abs(flux_p(u_)), abs(flux_p(v_))) *(v_-u_)
+
+   return
+end function rusanov
--- first_steps/FV_scal.F90
+++ first_steps/FV_scal.F90
@@ -9,12 +9,20 @@ program FiniteVolume
       real              :: flux
       end function flux
    end interface
+   
+   interface
+      function flux_p(u_)
+      real, intent (in) :: u_
+      real              :: flux_p
+      end function flux_p
+   end interface
+
 
    interface
-      function godunov(u_,v_)
+      function rusanov(u_,v_)
       real, intent (in) :: u_,v_
-      real              :: godunov
-      end function godunov
+      real              :: rusanov
+      end function rusanov
    end interface
 
 ! loop int   
@@ -22,17 +30,17 @@ program FiniteVolume
 
 
 !  declaration Riemann problem
-   real :: ud = -1, ug = 2
+   real :: ud = 1, ug = -2
 ! print *, 'declarer les valeurs gauche et droite du probleme de Riemann'
 ! read *, ud,ug 
 
 !  domaine spatial
-   integer, parameter   :: nx=10,  L=2
+   integer, parameter   :: nx=100,  L=2
    real, parameter      :: dx = real(L)/nx
    real, dimension(nx)  :: X(0:nx-1) = (/ ((i+0.5)*dx, i = 0,nx-1)  /)
 
 !  domaine temporelle
-   integer, parameter   :: nt=100,   T=1
+   integer, parameter   :: nt=1000,   T=1.5
    real                 :: t_=0.0,  dt=0.0
 
 !  Flux Numeriques
@@ -51,9 +59,9 @@ program FiniteVolume
 
 !  INIT
    where(X<real(L)/2)
-      U = ud
-   elsewhere 
       U = ug
+   elsewhere 
+      U = ud
    end where
 
    ! print *, "init"
@@ -64,7 +72,7 @@ program FiniteVolume
       n = n +1
 
       do i=1,nx
-         if(   abs(flux((U(i))))>vitesse )   vitesse = abs(flux((U(i))))
+         if(   abs(flux_p((U(i))))>vitesse )   vitesse = abs(flux_p((U(i))))
       end do
 
       ! print *, "vitesse max found : ", vitesse, ";"
@@ -81,15 +89,15 @@ program FiniteVolume
       do i=0,nx-1
          
         if(i==0) then   
-               FG(0) = godunov(U(0),U(0))
+               FG(0) = rusanov(U(0),U(0))
         else   
-               FG(i) = godunov(U(i-1),U(i))
+               FG(i) = rusanov(U(i-1),U(i))
         end if
         
         if(i==nx-1) then   
-               FD(nx-1) = godunov(U(nx-1),U(nx-1))
+               FD(nx-1) = rusanov(U(nx-1),U(nx-1))
         else   
-               FD(i)  = godunov(U(i),U(i+1)) 
+               FD(i)  = rusanov(U(i),U(i+1)) 
         end if
       end do
 
