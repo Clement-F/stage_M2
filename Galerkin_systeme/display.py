2077717c01fb1d39103eebaf2e16fadbec764b6a
zephyr <clement.fontan@outlook.com>
2026-08-29 14:03:05 +0200

    weird

--- Galerkin_systeme/display.py
+++ Galerkin_systeme/display.py
@@ -13,12 +13,11 @@ ordert = int(lines[2][10:16])
 nb_cell =int(lines[3][10:16]) ; nb_sub= int(lines[4][13:19])
 nx = nb_cell *nb_sub
 
-# sm = int(lines[5][5:11])
-sm =100
+sm = int(lines[5][5:11])
 
 T=[]
-# for k in range(6,6+sm):
-#    T.append(float(lines[k][14:30]))
+for k in range(6,6+sm):
+   T.append(float(lines[k][14:30]))
 
 
 #   solDG   SolSub   file_sol    
@@ -53,13 +52,13 @@ for k in range(0,sm,1):
         if(nb_var >2) :
             U_t[k][i][2]   = lines1[k*(nx+1) +i][44:58]
             
-        if((i)%(nb_sub)==0) :
-            X_cell[int((i)/(nb_sub))] =lines1[k*(nx+1) + i][0:10]
-            U_cell[k][int((i)/(nb_sub))-1][0] = sum(U_t[k,i-nb_sub:i,0])/nb_sub
-            X_midcell[int((i)/(nb_sub))] = (X_cell[int(i/(nb_sub))]+X_cell[int(i/(nb_sub))+1])/2
+        # if(i%nb_sub==0) : 
+        #     X_cell[int(i/nb_sub)] =lines1[k*(nx+1) + i][59:69]
+        #     U_cell[k][int(i/nb_sub)-1][0] = sum(U_t[k,i-nb_sub:i,0])/nb_sub
+        #     X_midcell[int(i/nb_sub)] = (X_cell[int(i/nb_sub)]+X_cell[int(i/nb_sub)+1])/2
             
     
-        #U_ex[k][i][0]   = lines2[k*(nx+1) +i][11:27]
+        # U_ex[k][i][0]   = lines2[k*(nx+1) +i][11:27]
         # if(nb_var >1) : 
         #      U_ex[k][i][1]   = lines2[k*(nx+1) +i][28:43]
         # if(nb_var >2) :
@@ -72,8 +71,8 @@ for k in range(0,sm,1):
                   
     for i in range(0,nb_cell+1): 
         1+1
-        plt.plot([X_cell[i],X_cell[i]],[-1,2],linestyle='--', color='gray')
-    # plt.plot(X_midcell,U_cell[k,:,0], marker='.')
+        #plt.plot([X_cell[i],X_cell[i]],[-1,2],linestyle='--', color='gray')
+    #plt.plot(X_midcell,U_cell[k,:,0], marker='.')
     #plt.plot(X,U_t[0,:,0],'g-')
     plt.plot(X,U_t[k,:,0] ,'b-')
     plt.plot(X,U_ex[k,:,0],'g-')
@@ -83,24 +82,23 @@ for k in range(0,sm,1):
     if(nb_var>1):
         1+1
         #plt.plot(X,U_ex[k,:,1],'k-')
-        plt.plot(X,U_t[k,:,1],'r-', marker='.')
+        #plt.plot(X,U_t[k,:,1],'r-', marker='.')
         #plt.plot(X,abs(U_t[k,:,1]-U_ex[k,:,1]),'k-')
         #plt.plot(X,U_t[k,:,1]/U_t[k,:,0],'r-', marker='.')
     if(nb_var>2):
         1+1
         #plt.plot(X,U_ex[k,:,2],'k-')
-        plt.plot(X,U_t[k,:,2],'g-', marker='.')
+        #plt.plot(X,U_t[k,:,2],'g-', marker='.')
         #plt.plot(X,abs(U_t[k,:,2]-U_ex[k,:,2]),'k-')
     
     #plt.plot(X,P_[k,:],'k-.')
-    #print(min(P_[k,:]))
+    print(min(P_[k,:]))
     #plt.plot(X,P2_[k,:],'g-.')
     #m=-.2; M=1.2
     
     #plt.ylim(3.8,4.8); plt.xlim(0.2,0.8)
    # plt.ylim(m,M);plt.xlim(-0.,1.)
     #plt.ylim(-.2, 7.2)
-    # plt.title("time = "+str(T[k]))
     plt.show()    
     # plt.savefig("save_"+str(T[k])+".png")    
     # plt.cla() 
